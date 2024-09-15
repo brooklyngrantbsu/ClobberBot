8f38f56316f7e13e17a7abc4e970d331976d7b2d
Megan-Aker <89935925+Megan-Aker@users.noreply.github.com>
2024-09-15 16:33:22 -0600

    fixes the shooting

--- brooklyngrant.java
+++ brooklyngrant.java
@@ -8,10 +8,10 @@ import java.io.IOException;
 
 public class brooklyngrant extends ClobberBot {
     private BufferedImage myImage;
-    private static final double SAFE_DISTANCE = 50.0; // Distance to maintain from bullets and bots
-
+    
     public brooklyngrant(Clobber game) {
         super(game);
+
         try {
             myImage = ImageIO.read(new File("deathstar.png"));
         } catch (IOException e) {
@@ -21,100 +21,97 @@ public class brooklyngrant extends ClobberBot {
 
     ClobberBotAction currAction;
 
-    public ClobberBotAction takeTurn(WhatIKnow currState) {
-        long startTime = System.currentTimeMillis();
+    private final int SAFE_DISTANCE = 50; 
+    private final int SHOOT_DISTANCE = 150; 
 
-        Point2D myPos = currState.me;
+    public ClobberBotAction takeTurn(WhatIKnow currState) {
+        Point2D me = currState.me;
         Vector<BulletPoint2D> bullets = currState.bullets;
         Vector<BotPoint2D> bots = currState.bots;
 
-        // Avoid bullets
-        Point2D avoidVector = calculateAvoidanceVector(bullets, myPos);
-
-        // Decide movement or shooting
-        if (shouldShoot(currState)) {
-            currAction = new ClobberBotAction(1, ClobberBotAction.SHOOT);
-        } else {
-            currAction = decideMovement(avoidVector, myPos);
+        BulletPoint2D nearestBullet = findNearestBullet(me, bullets);
+        if (nearestBullet != null && me.distance(nearestBullet) < SAFE_DISTANCE) {
+            currAction = moveAwayFromBullet(me, nearestBullet);
+            return currAction;
         }
 
-        long endTime = System.currentTimeMillis();
-        long elapsedTime = endTime - startTime;
-
-        if (elapsedTime > 10) { // if time is bigger than 10ms (1/100 of a second)
-            System.out.println("TOO SLOW: Elapsed time: " + elapsedTime + " milliseconds");
+        BotPoint2D nearestBot = findNearestBot(me, bots);
+        if (nearestBot != null && me.distance(nearestBot) < SHOOT_DISTANCE) {
+            currAction = shootAtBot(me, nearestBot);
+            return currAction;
         }
 
+        if (currAction == null || ((currAction.getAction() & ClobberBotAction.SHOOT) > 0) || rand.nextInt(10) > 8) {
+            switch (rand.nextInt(8)) {
+                case 0: currAction = new ClobberBotAction(rand.nextInt(2) + 1, ClobberBotAction.UP); break;
+                case 1: currAction = new ClobberBotAction(rand.nextInt(2) + 1, ClobberBotAction.DOWN); break;
+                case 2: currAction = new ClobberBotAction(rand.nextInt(2) + 1, ClobberBotAction.LEFT); break;
+                case 3: currAction = new ClobberBotAction(rand.nextInt(2) + 1, ClobberBotAction.RIGHT); break;
+                case 4: currAction = new ClobberBotAction(rand.nextInt(2) + 1, ClobberBotAction.UP | ClobberBotAction.LEFT); break;
+                case 5: currAction = new ClobberBotAction(rand.nextInt(2) + 1, ClobberBotAction.UP | ClobberBotAction.RIGHT); break;
+                case 6: currAction = new ClobberBotAction(rand.nextInt(2) + 1, ClobberBotAction.DOWN | ClobberBotAction.LEFT); break;
+                default: currAction = new ClobberBotAction(rand.nextInt(2) + 1, ClobberBotAction.DOWN | ClobberBotAction.RIGHT); break;
+            }
+        }
         return currAction;
     }
 
-    private Point2D calculateAvoidanceVector(Vector<BulletPoint2D> bullets, Point2D myPos) {
-        double xAvoid = 0;
-        double yAvoid = 0;
-
+    private BulletPoint2D findNearestBullet(Point2D me, Vector<BulletPoint2D> bullets) {
+        BulletPoint2D nearest = null;
+        double minDistance = Double.MAX_VALUE;
         for (BulletPoint2D bullet : bullets) {
-            double distance = myPos.distance(bullet);
-            if (distance < SAFE_DISTANCE) {
-                // Repel from the bullet
-                xAvoid += myPos.getX() - bullet.getX();
-                yAvoid += myPos.getY() - bullet.getY();
+            double distance = me.distance(bullet);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearest = bullet;
             }
         }
-        return new Point2D.Double(xAvoid, yAvoid);
+        return nearest;
     }
 
-    private boolean shouldShoot(WhatIKnow currState) {
-        // Basic strategy: shoot if any bot is in a nearby range
-        Point2D myPos = currState.me;
-        for (BotPoint2D bot : currState.bots) {
-            double distance = myPos.distance(bot);
-            if (distance < SAFE_DISTANCE) {
-                return true; // Target acquired, shoot
-            }
-        }
-        return false;
-    }
-
-    private ClobberBotAction decideMovement(Point2D avoidVector, Point2D myPos) {
-        if (avoidVector.distance(0, 0) > 0) {
-            // Move in the direction away from bullets
-            return determineMoveDirection(avoidVector);
+    private ClobberBotAction moveAwayFromBullet(Point2D me, BulletPoint2D bullet) {
+        double dx = me.getX() - bullet.getX();
+        double dy = me.getY() - bullet.getY();
+        if (Math.abs(dx) > Math.abs(dy)) {
+            return new ClobberBotAction(1, dx > 0 ? ClobberBotAction.RIGHT : ClobberBotAction.LEFT);
         } else {
-            // Random move when there is no immediate danger
-            return randomMove();
+            return new ClobberBotAction(1, dy > 0 ? ClobberBotAction.DOWN : ClobberBotAction.UP);
         }
     }
 
-    private ClobberBotAction determineMoveDirection(Point2D avoidVector) {
-        // Determine the best move direction based on avoidance vector
-        if (Math.abs(avoidVector.getX()) > Math.abs(avoidVector.getY())) {
-            if (avoidVector.getX() > 0) {
-                return new ClobberBotAction(1, ClobberBotAction.RIGHT);
-            } else {
-                return new ClobberBotAction(1, ClobberBotAction.LEFT);
+    private BotPoint2D findNearestBot(Point2D me, Vector<BotPoint2D> bots) {
+        BotPoint2D nearest = null;
+        double minDistance = Double.MAX_VALUE;
+        for (BotPoint2D bot : bots) {
+            double distance = me.distance(bot);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearest = bot;
             }
+        }
+        return nearest;
+    }
+
+    // private ClobberBotAction shootAtBot(Point2D me, BotPoint2D bot) {
+    //     double dx = bot.getX() - me.getX();
+    //     double dy = bot.getY() - me.getY();
+    //     if (Math.abs(dx) > Math.abs(dy)) {
+    //         return new ClobberBotAction(1, dx > 0 ? ClobberBotAction.SHOOT_RIGHT : ClobberBotAction.SHOOT_LEFT);
+    //     } else {
+    //         return new ClobberBotAction(1, dy > 0 ? ClobberBotAction.SHOOT_DOWN : ClobberBotAction.SHOOT_UP);
+    //     }
+    // }
+
+    private ClobberBotAction shootAtBot(Point2D me, BotPoint2D bot) {
+        double dx = bot.getX() - me.getX();
+        double dy = bot.getY() - me.getY();
+        if (Math.abs(dx) > Math.abs(dy)) {
+            return new ClobberBotAction(1, dx > 0 ? (ClobberBotAction.SHOOT | ClobberBotAction.RIGHT) : (ClobberBotAction.SHOOT | ClobberBotAction.LEFT));
         } else {
-            if (avoidVector.getY() > 0) {
-                return new ClobberBotAction(1, ClobberBotAction.DOWN);
-            } else {
-                return new ClobberBotAction(1, ClobberBotAction.UP);
-            }
+            return new ClobberBotAction(1, dy > 0 ? (ClobberBotAction.SHOOT | ClobberBotAction.DOWN) : (ClobberBotAction.SHOOT | ClobberBotAction.UP));
         }
     }
-
-    private ClobberBotAction randomMove() {
-        switch (rand.nextInt(4)) {
-            case 0:
-                return new ClobberBotAction(1, ClobberBotAction.UP);
-            case 1:
-                return new ClobberBotAction(1, ClobberBotAction.DOWN);
-            case 2:
-                return new ClobberBotAction(1, ClobberBotAction.LEFT);
-            case 3:
-            default:
-                return new ClobberBotAction(1, ClobberBotAction.RIGHT);
-        }
-    }
+    
 
     public void drawMe(Graphics page, Point2D me) {
         int x, y;
@@ -130,4 +127,3 @@ public class brooklyngrant extends ClobberBot {
         return "Brooklyn Grant";
     }
 }
-
