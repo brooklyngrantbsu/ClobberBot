97b4371bda99f6576a07c503d24c88d09add833c
brooklyngrantbsu <brooklyngrant@u.boisestate.edu>
2024-09-16 12:10:44 -0600

    Better shooting with velocity

--- brooklyngrant.java
+++ brooklyngrant.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 public class brooklyngrant extends ClobberBot {
     private BufferedImage myImage;
+    private Map<BotPoint2D, Point2D> previousPositions = new HashMap<>();
     
     public brooklyngrant(Clobber game) {
         super(game);
@@ -28,20 +29,23 @@ public class brooklyngrant extends ClobberBot {
         Point2D me = currState.me;
         Vector<BulletPoint2D> bullets = currState.bullets;
         Vector<BotPoint2D> bots = currState.bots;
-
+    
         BulletPoint2D nearestBullet = findNearestBullet(me, bullets);
         if (nearestBullet != null && me.distance(nearestBullet) < SAFE_DISTANCE) {
             currAction = moveAwayFromBullet(me, nearestBullet);
             return currAction;
         }
-
+    
         BotPoint2D nearestBot = findNearestBot(me, bots);
         if (nearestBot != null && me.distance(nearestBot) < SHOOT_DISTANCE) {
             currAction = shootAtBot(me, nearestBot);
             return currAction;
         }
-
+    
+        updatePreviousPositions(bots); //update past bots position
+    
         if (currAction == null || ((currAction.getAction() & ClobberBotAction.SHOOT) > 0) || rand.nextInt(10) > 8) {
+            // random logic
             switch (rand.nextInt(8)) {
                 case 0: currAction = new ClobberBotAction(rand.nextInt(2) + 1, ClobberBotAction.UP); break;
                 case 1: currAction = new ClobberBotAction(rand.nextInt(2) + 1, ClobberBotAction.DOWN); break;
@@ -55,6 +59,12 @@ public class brooklyngrant extends ClobberBot {
         }
         return currAction;
     }
+    
+    private void updatePreviousPositions(Vector<BotPoint2D> bots) {
+        for (BotPoint2D bot : bots) {
+            previousPositions.put(bot, new Point2D.Double(bot.getX(), bot.getY()));
+        }
+    }    
 
     private BulletPoint2D findNearestBullet(Point2D me, Vector<BulletPoint2D> bullets) {
         BulletPoint2D nearest = null;
@@ -92,23 +102,33 @@ public class brooklyngrant extends ClobberBot {
         return nearest;
     }
 
-    // private ClobberBotAction shootAtBot(Point2D me, BotPoint2D bot) {
-    //     double dx = bot.getX() - me.getX();
-    //     double dy = bot.getY() - me.getY();
-    //     if (Math.abs(dx) > Math.abs(dy)) {
-    //         return new ClobberBotAction(1, dx > 0 ? ClobberBotAction.SHOOT_RIGHT : ClobberBotAction.SHOOT_LEFT);
-    //     } else {
-    //         return new ClobberBotAction(1, dy > 0 ? ClobberBotAction.SHOOT_DOWN : ClobberBotAction.SHOOT_UP);
-    //     }
-    // }
-
     private ClobberBotAction shootAtBot(Point2D me, BotPoint2D bot) {
-        double dx = bot.getX() - me.getX();
-        double dy = bot.getY() - me.getY();
-        if (Math.abs(dx) > Math.abs(dy)) {
-            return new ClobberBotAction(1, dx > 0 ? (ClobberBotAction.SHOOT | ClobberBotAction.RIGHT) : (ClobberBotAction.SHOOT | ClobberBotAction.LEFT));
+        Point2D previousPosition = previousPositions.get(bot);
+        double botVelocityX = 0;
+        double botVelocityY = 0;
+    
+        // calculate velocity of bot if we have old positions to predict moves
+        if (previousPosition != null) {
+            botVelocityX = bot.getX() - previousPosition.getX();
+            botVelocityY = bot.getY() - previousPosition.getY();
+        }
+    
+        // estimate time to impact (using Euclidean distance)
+        double distance = me.distance(bot);
+        double timeToImpact = distance;
+    
+        // predict the bot's future position
+        double predictedX = bot.getX() + botVelocityX * timeToImpact;
+        double predictedY = bot.getY() + botVelocityY * timeToImpact;
+    
+        // shoot this way
+        double predictDx = predictedX - me.getX();
+        double predictDy = predictedY - me.getY();
+    
+        if (Math.abs(predictDx) > Math.abs(predictDy)) {
+            return new ClobberBotAction(1, predictDx > 0 ? (ClobberBotAction.SHOOT | ClobberBotAction.RIGHT) : (ClobberBotAction.SHOOT | ClobberBotAction.LEFT));
         } else {
-            return new ClobberBotAction(1, dy > 0 ? (ClobberBotAction.SHOOT | ClobberBotAction.DOWN) : (ClobberBotAction.SHOOT | ClobberBotAction.UP));
+            return new ClobberBotAction(1, predictDy > 0 ? (ClobberBotAction.SHOOT | ClobberBotAction.DOWN) : (ClobberBotAction.SHOOT | ClobberBotAction.UP));
         }
     }
     
